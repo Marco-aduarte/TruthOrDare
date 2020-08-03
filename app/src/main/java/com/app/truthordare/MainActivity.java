6bec6958671d20b59db8f748eeed29240370d026
Marco <marcoantoniotd@gmail.com>
2020-08-03 11:04:18 +0100

    second

--- app/src/main/java/com/app/truthordare/MainActivity.java
+++ app/src/main/java/com/app/truthordare/MainActivity.java
@@ -1,14 +1,16 @@
 package com.app.truthordare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.constraintlayout.solver.widgets.ChainHead;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
