a77ab99955a114835cec4c35c0c2c5f61ed164f8
Marco <marcoantoniotd@gmail.com>
2020-08-04 01:00:08 +0100

    Paginas dos botões e layout da pag principal

---
+++ app/src/main/java/com/app/truthordare/Dirty_Activity.java
@@ -0,0 +1,14 @@
+package com.app.truthordare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dirty_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dirty_);
+    }
+}
