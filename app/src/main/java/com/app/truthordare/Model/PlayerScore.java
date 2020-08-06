a47973378bde65b92aeb5b807bf6160931c0e296
sdrrv <duartecelvas@gmail.com>
2020-08-06 09:06:21 +0100

    Added the rounds to the score board class

--- app/src/main/java/com/app/truthordare/Model/PlayerScore.java
+++ app/src/main/java/com/app/truthordare/Model/PlayerScore.java
@@ -4,19 +4,36 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class PlayerScore {
-    private Hashtable <String, Player> scoreBoard;
     private ArrayList <Player> playersArray;
+    private int current_player = 0;
+    private int rounds;
+    private int current_round = 1;
 
-    public PlayerScore(){
-        scoreBoard = new Hashtable<>();
+    public PlayerScore(int rounds){
         playersArray = new ArrayList<>();
+        this.rounds = rounds;
     }
-    public void add_player(Player player){
-        scoreBoard.put(player.getName(), player);
+    private void add_player(Player player){
         playersArray.add(player);
     }
-    public Player get_player(String player_name){
-        return scoreBoard.get(player_name);
+
+    public void add_all_players(ArrayList<String> players){
+        for (String player : players)
+            add_player(new Player(player));
+    }
+
+    public Player next_Player(){
+        if(current_player == playersArray.size()) {
+            current_player = 0;
+            current_round++;
+        }
+        if(current_round > rounds)
+            return null;
+        return playersArray.get(current_player++);
+    }
+
+    public int get_round(){
+        return current_round;
     }
 
     private boolean less(Player x, Player y){
